630f67d02910c5693da74718e243a7968cf0f856
ZarathosDeath <nando_rhapsody@hotmail.com>
2019-05-18 11:10:51 -0300

    add action to search on stackoverflow Co-authored-by: Carlos Aragon <carloseraragon@gmail.com>

--- bot/actions/actions.py
+++ bot/actions/actions.py
@@ -1,6 +1,7 @@
 from rasa_core_sdk import Action
+import re
 # from rasa_core_sdk.events import SlotSet
-# import requests
+import requests
 # import random
 
 
@@ -20,3 +21,17 @@ class ActionOTRS(Action):
 
     def closeTicket(self, dispatcher, tracker, domain):
         return
+    
+class ActionSearchOnStackoverflow(Action):
+
+    def name(self):
+        return "action_search_on_stackoverflow"
+
+    def run(self, dispatcher, tracker, domain):
+        question = re.search(r'(buscar|pesquisar)\s(.*)(no)*\s*([sS]tack\s?[oO]verflow)', tracker.latest_message.text).group[2]
+        URL = 'https://api.stackexchange.com/docs/advanced-search#order=desc&sort=relevance&accepted=True&tagged=python%3B'+ question + '&filter=default&site=stackoverflow'
+        res = requests.get(url = URL)
+        data = res.json()
+        dispatcher.utter_message('Aqui está: ' + data[0].link)
+        return 
+    
