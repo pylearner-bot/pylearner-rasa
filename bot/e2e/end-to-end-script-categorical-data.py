0b40de6b4fcd60c35ec2a9bf4370c36cd6d7491e
Bruno <brunnohenriq697@gmail.com>
2019-06-07 18:24:01 -0300

    Categorical Data Test Report

---
+++ bot/e2e/end-to-end-script-categorical-data.py
@@ -0,0 +1,60 @@
+"""
+End to end test script
+To add new test cases add the:
+    - user_intent
+    - user_input
+    - utter
+"""
+
+user_intent = [
+    'transformar_dados_categoricos',
+    'categoricos_rapidos',
+    'categoricos_rapidos2',
+    'categoricos_rapidos3',
+    'categoricos_rapidos4',
+    'categoricos_rapidos5'
+]
+
+user_input = [
+    'tratar dados categoricos',
+    'scikit-learn',
+    'label encoder',
+    'fit transorform',
+    'inverse transform',
+    'instanciar'
+    ]
+
+utter = [
+    'utter_transformar_dados_categoricos',
+    'utter_categoricos_rapidos',
+    'utter_categoricos_rapidos2',
+    'utter_categoricos_rapidos3',
+    'utter_categoricos_rapidos4',
+    'utter_categoricos_rapidos5'
+    ]
+
+
+def create_stories(user_intent, user_input, utter):
+    """
+    Zip through lists and return them in the e2e test format
+    """
+    for intent, inp, utt in zip(user_intent, user_input, utter):
+        yield "* {}: {}\n\t - {}".format(intent, inp, utt)
+
+
+def write_file(stories):
+    """
+    write story tests to file
+    """
+    E2E_FILE = './e2e_stories_categorical_data.md'
+    title = "## Stories for Categorical Data Pyter Test\n"
+
+    with open(E2E_FILE, "w") as f:
+        f.write(title)
+        stories = [story for story in stories]
+        for story in stories:
+            f.write("%s\n" % story)
+
+
+stories = create_stories(user_intent, user_input, utter)
+write_file(stories)
---
+++ bot/results/categorical_failed_stories.md
@@ -0,0 +1,15 @@
+## Stories for Categorical Data Pyter Test
+* transformar_dados_categoricos: tratar dados categoricos
+    - utter_transformar_dados_categoricos
+* categoricos_rapidos: scikit-learn
+    - utter_categoricos_rapidos   <!-- predicted: action_default_fallback -->
+* categoricos_rapidos2: label encoder   <!-- predicted: categoricos_rapidos5: label encoder -->
+    - utter_categoricos_rapidos2   <!-- predicted: action_default_fallback -->
+* categoricos_rapidos3: fit transorform   <!-- predicted: None: fit transorform -->
+    - utter_categoricos_rapidos3   <!-- predicted: action_default_fallback -->
+* categoricos_rapidos4: inverse transform   <!-- predicted: None: inverse transform -->
+    - utter_categoricos_rapidos4   <!-- predicted: action_default_fallback -->
+* categoricos_rapidos5: instanciar
+    - utter_categoricos_rapidos5   <!-- predicted: action_default_fallback -->
+
+
