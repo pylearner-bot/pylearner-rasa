644c4f5dcde6f82b07a368ef7caf3000d656dd62
Bruno <brunnohenriq697@gmail.com>
2019-06-08 05:29:53 -0300

    Solve and Update Action Code to Bot Response Co-authored-by: Carlos Aragon <carloseraragon@gmail.com> Co-authored-by: ZarathosDeath <nando_rhapsody@hotmail.com>

--- bot/actions/actions.py
+++ bot/actions/actions.py
@@ -50,9 +50,9 @@ class SearchOnStackoverflow(Action):
             dispatcher.utter_message('Tente escrever desta forma:')
             dispatcher.utter_message('pesquisar [sua dúvida] no stackoverflow')
     
-    def validate_answered(self, dictionary):
+    def validate_answered(self, data):
         links = []
-        for item in dictionary['items']:
+        for item in data['items']:
             if str(item['is_answered']) == 'True':
                 links.append(item['link'])
             if len(links) == 3:
@@ -65,18 +65,9 @@ class SearchOnCrossValidated(Action):
     def name(self):
         return "action_search_on_crossvalidated"
 
-    def validate_answered(self, dictionary):
-        links = []
-        for item in dictionary['items']:
-            if str(item['is_answered']) == 'True':
-                links.append(item['link'])
-            if len(links) == 3:
-                break
-        return links
-
     def run(self, dispatcher, tracker, domain):
         question = ''
-        if re.search(r'(buscar|pesquisar)\s(.*)no*\s*([sS]tack\s?[oO]verflow)', tracker.latest_message['text']):
+        if re.search(r'(buscar|pesquisar)\s(.*)no*\s*([cC]ross\s?[vV]alidated)', tracker.latest_message['text']):
             question = re.search(r'(buscar|pesquisar)\s(.*)no*\s*([cC]ross\s?[vV]alidated)', tracker.latest_message['text']).group(2)
 
         url = 'https://api.stackexchange.com/2.2/search'
@@ -98,7 +89,16 @@ class SearchOnCrossValidated(Action):
                 # botResponse = 'Aqui está: ' + data['items'][0]['link']
                 dispatcher.utter_message(link)
         else:
-            botResponse = 'Infelizmente não encontrei nada sobre ' + question +' no CrossValidated. Tente escrever de forma mais compacta e em inglês, para refinar a busca!'
+            botResponse = 'Infelizmente não encontrei nada sobre ' + question +' no StackOverflow. Tente escrever de forma mais compacta e em inglês,             para refinar a busca!'
             dispatcher.utter_message(botResponse)
             dispatcher.utter_message('Tente escrever desta forma:')
-            dispatcher.utter_message('pesquisar [sua dúvida] no crossvalidated')
+            dispatcher.utter_message('pesquisar [sua dúvida] no stackoverflow')
+
+    def validate_answered(self, data):
+        links = []
+        for item in data['items']:
+            if str(item['is_answered']) == 'True':
+                links.append(item['link'])
+            if len(links) == 3:
+                break
+        return links
